8838d2412d5b1a50a3427a3c75ae9cb768384351
pmadri <pedrophacv@gmail.com>
2025-03-14 20:31:22 -0300

    ínicio do java

---
+++ main.js
@@ -0,0 +1,5 @@
+// Simulaçao de adição de produtos à cesta do site //
+const botoesAdicionar = document.querySelectorAll(".adicionar");
+const listaPedido = document.getElementById("lista-pedido");
+const totalElemento = document.getElementById("total");
+let total = 0;
